c7ec55887a3cf1d7352265d9645b9dbc405915f7
theo <theo@jlab.org>
2025-03-12 15:07:54 -0400

    tweak css so that main content fills available space

--- resources/views/psslog/index.blade.php
+++ resources/views/psslog/index.blade.php
@@ -2,8 +2,7 @@
 
 @section('main')
     <div class="flex flex-wrap w-100">
-{{--        {{dd($filters["date"])}}--}}
-        <main class="p-4 mt-14 w-full sm:w-2/3 md:w-3/4">
+        <main class="p-4 mt-14 flex-grow">
             <!-- The table of open accesses -->
             <div id="open-accesses-container" hx-get="{{route('accesses.open')}}" hx-trigger="every 1m" hx-swap="innerHTML">
             @if ($accesses->count() > 0)
@@ -19,12 +18,11 @@
 
         <!-- Sidebar -->
         <aside
-            class="right-0 w-86 pt-14  bg-white dark:bg-gray-800 dark:border-gray-700"
+            class="right-0 min-w-84 pt-14  bg-white dark:bg-gray-800 dark:border-gray-700"
             aria-label="Sidenav"
             id="drawer-navigation"
         >
             <div class="overflow-y-auto py-5 px-3 h-full bg-white dark:bg-gray-800">
-                @include('layouts.sidebar_datepicker')
                 @include('layouts.sidebar_filters')
                 @include('layouts.sidebar_links')
             </div>
